8c5bdc71fc140405231d9c397fbca156cbe392dd
sepryandayon810-debug <sepryandayon810@gmail.com>
2026-04-30 10:25:13 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,13 +5,14 @@
  */
 
 const firebaseConfig = {
-    apiKey: "__API_KEY__",
-    authDomain: "__PROJECT_ID__.firebaseapp.com",
-    databaseURL: "https://__PROJECT_ID__-default-rtdb.firebaseio.com",
-    projectId: "__PROJECT_ID__",
-    storageBucket: "__PROJECT_ID__.appspot.com",
-    messagingSenderId: "__SENDER_ID__",
-    appId: "__APP_ID__"
+    apiKey: "AIzaSyCSC05MTnaiiSftj1TA-LVCH4ymHBAbkoU",
+    authDomain: "hifzicell-v2.firebaseapp.com",
+    databaseURL: "https://hifzicell-v2-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "hifzicell-v2",
+    storageBucket: "hifzicell-v2.firebasestorage.app",
+    messagingSenderId: "766095621773",
+    appId: "1:766095621773:web:da40a79eab0f573683acc4",
+    measurementId: "G-N9B9PKFCHT"
 };
 
 // Initialize Firebase (hanya sekali)
